c585bf725eea5fd3cb4bb650b390649cfb5ed56e
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-11-20 11:38:23 -0500

    Add eventos y estilos del calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,12 @@
 
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
 import enUS from 'date-fns/locale/en-US'
 
 import { Navbar } from "../"
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
 
 const locales = {
   'en-US': enUS,
@@ -19,11 +21,17 @@ const localizer = dateFnsLocalizer({
 });
 
 
-const myEventsList = [{
+const events = [{
   title: 'Cumpleanios del Jefe',
   notes: 'Hay que comprar el pastel',
   start: new Date(),
-  end: addHours
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Nestor'
+  }
+
 }]
 
 export const CalendarPage = () => {
@@ -33,10 +41,10 @@ export const CalendarPage = () => {
 
       <Calendar
         localizer={localizer}
-        events={myEventsList}
+        events={events}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 500 }}
+        style={{ height: 'calc( 100vh - 80px )' }}
       />
     
 )
